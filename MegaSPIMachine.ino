8837a9d1f3598e5a13b48e6cc06827caf9d0028f
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-11-17 11:07:20 -0800

    New design to make it possible to hook up 12 spi devices

--- MegaSPIMachine.ino
+++ MegaSPIMachine.ino
@@ -4,11 +4,17 @@
 // The address is 
 #include <SPI.h>
 #include <libbonuspin.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-constexpr auto EnablePin0 = 2;
-constexpr auto EnablePin1 = 3;
-constexpr auto EnablePin2 = 4;
-constexpr auto ActivatorPin = 5;
+constexpr auto CSPin0 = 2;
+constexpr auto CSPin1 = 3;
+constexpr auto CSPin2 = 4;
+constexpr auto CSPin3 = 5;
+constexpr auto EnablePin = 6;
+constexpr auto OLED_RESET = 7;
+
+Adafruit_SSD1306 display(OLED_RESET);
 
 enum class SRAMOpcodes : uint8_t {
     RDSR = 0x05,
@@ -33,13 +39,20 @@ void transferAddress(uint32_t address) {
     SPI.transfer(uint8_t(address));
 }
 
-bonuspin::HC138<2, 3, 4> decoder;
+void setDecoderPins(byte index) {
+    auto masked = index & 0xF;
+    digitalWrite(CSPin0, masked & 1 ? HIGH : LOW);
+    digitalWrite(CSPin1, masked & 2 ? HIGH : LOW);
+    digitalWrite(CSPin2, masked & 4 ? HIGH : LOW);
+    digitalWrite(CSPin3, masked & 8 ? HIGH : LOW);
+}
+
 constexpr uint8_t computeChipIndex(uint32_t value) noexcept {
-    return (0b1110'0000'0000'0000'0000 & value) >> 17;
+    return (0b1'1110'0000'0000'0000'0000 & value) >> 17;
 }
 template<int pin>
 uint8_t readRam(uint32_t address) {
-    decoder.enableLine(computeChipIndex(address));
+    setDecoderPins(computeChipIndex(address));
     CableSelectHolder<pin> holder;
     sendOpcode(SRAMOpcodes::READ);
     transferAddress(address);
@@ -48,44 +61,66 @@ uint8_t readRam(uint32_t address) {
 }
 template<int pin>
 void writeRam(uint32_t address, uint8_t value) {
-    decoder.enableLine(computeChipIndex(address));
+    setDecoderPins(computeChipIndex(address));
     CableSelectHolder<pin> holder;
     sendOpcode(SRAMOpcodes::WRITE);
     transferAddress(address);
     SPI.transfer(value);
 }
-int32_t chipIndex = -1;
-constexpr auto NumberOfChips = 5;
+uint32_t addr = 0;
 void setup() {
-    Serial.begin(115200);
-    pinMode(ActivatorPin, OUTPUT);
-    pinMode(ActivatorPin, HIGH);
+    addr = 0;
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+    pinMode(CSPin0, OUTPUT);
+    pinMode(CSPin1, OUTPUT);
+    pinMode(CSPin2, OUTPUT);
+    pinMode(CSPin3, OUTPUT);
+    pinMode(EnablePin, OUTPUT);
+    digitalWrite(CSPin0, LOW);
+    digitalWrite(CSPin1, LOW);
+    digitalWrite(CSPin2, LOW);
+    digitalWrite(CSPin3, LOW);
+    digitalWrite(EnablePin, HIGH);
     SPI.begin();
-    // only have 5 devices connected right now so D
-    for (uint32_t i = 0; i < 0x10'0000; ++i) {
-        if (auto newChipIndex = computeChipIndex(i); chipIndex != newChipIndex) {
-            if (newChipIndex >= NumberOfChips) {
-                break;
-            }
-            Serial.print("Testing chip: ");
-            Serial.println(newChipIndex, HEX);
-            chipIndex = newChipIndex;
-        }
-        auto i8Val = static_cast<uint8_t>(0xFD);
-        writeRam<ActivatorPin>(i, i8Val);
-        if (auto result = readRam<ActivatorPin>(i); result != i8Val) {
-            Serial.print("Readback mismatch, Address: 0x");
-            Serial.print(i, HEX);
-            Serial.print(", Expected: 0x");
-            Serial.print(i8Val, HEX);
-            Serial.print(", Got: 0x");
-            Serial.println(result, HEX);
-        } 
-    }
-    Serial.println("Done");
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0,0);
+    display.println("Donuts");
+    display.display();
+    delay(1000);
+
 }
-
-
 void loop() {
-
+    if (addr < 0x18'0000) {
+        auto i8Val = static_cast<uint8_t>(addr);
+        //auto i8Val = static_cast<uint8_t>(0xFD);
+        writeRam<EnablePin>(addr, i8Val);
+        if (auto result = readRam<EnablePin>(addr); result != i8Val) {
+            display.clearDisplay();
+            display.setTextSize(1);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.print("cci: ");
+            display.println(computeChipIndex(addr), HEX);
+            display.print("addr: ");
+            display.println(addr, HEX);
+            display.print("expect: ");
+            display.println(i8Val, HEX);
+            display.print("got: ");
+            display.println(result, HEX);
+            display.display();
+            delay(5);
+        } else {
+            delay(1);
+        }
+        ++addr;
+    } else {
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setCursor(0,0);
+        display.println("DONE!");
+        display.display();
+        delay(100);
+    }
 }
